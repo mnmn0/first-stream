daeaf07e2d9cf009988ac4b7b76dc71f5d4bdc89
mnmn0 <minamino@mobamasu.com>
2025-01-03 22:56:41 +0900

    add api hooks

---
+++ src/hooks/use-http.ts
@@ -0,0 +1,197 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import useSWR, { SWRConfiguration } from 'swr';
+
+const api = axios.create({
+  baseURL: process.env.VITE_APP_API_ENDPOINT
+});
+
+const fetcher = async (url: string) => {
+  const res = await api.get(url);
+  return res.data;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const fetchWithParams = async ([url, params]: [string, Record<string, any>]) => {
+  const res = await api
+    .get(url, {
+      params,
+    });
+  return res.data;
+};
+
+/**
+ * Hooks for Http Request
+ * @returns
+ */
+const useHttp = () => {
+  // const alert = useAlertSnackbar();
+
+  return {
+    /**
+     * GET Request
+     * Implemented with SWR
+     * @param url
+     * @param config
+     * @returns
+     */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    get: <Data = any, Error = any>(
+      url: string | [string, ...unknown[]] | null,
+      config?: SWRConfiguration
+    ) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      return useSWR<Data, AxiosError<Error>>(
+        url,
+        typeof url === 'string' ? fetcher : fetchWithParams,
+        {
+          ...config,
+        }
+      );
+    },
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    getOnce: <RES = any, DATA = any>(
+      url: string,
+      params?: DATA,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      errorProcess?: (err: any) => void
+    ) => {
+      return new Promise<AxiosResponse<RES>>((resolve, reject) => {
+        api
+          .get<RES, AxiosResponse<RES>, DATA>(url, {
+            params,
+          })
+          .then((data) => {
+            resolve(data);
+          })
+          .catch((err) => {
+            if (errorProcess) {
+              errorProcess(err);
+            } else {
+              // alert.openError(getErrorMessage(err));
+            }
+            reject(err);
+          });
+      });
+    },
+
+    /**
+     * POST Request
+     * @param url
+     * @param data
+     * @param errorProcess
+     * @returns
+     */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    post: <RES = any, DATA = any>(
+      url: string,
+      data: DATA,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      errorProcess?: (err: any) => void
+    ) => {
+      return new Promise<AxiosResponse<RES>>((resolve, reject) => {
+        api
+          .post<RES, AxiosResponse<RES>, DATA>(url, data)
+          .then((data) => {
+            resolve(data);
+          })
+          .catch((err) => {
+            if (errorProcess) {
+              errorProcess(err);
+            } else {
+              // alert.openError(getErrorMessage(err));
+            }
+            reject(err);
+          });
+      });
+    },
+
+    /**
+     * PUT Request
+     * @param url
+     * @param data
+     * @param errorProcess
+     * @returns
+     */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    put: <RES = any, DATA = any>(
+      url: string,
+      data: DATA,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      errorProcess?: (err: any) => void
+    ) => {
+      return new Promise<AxiosResponse<RES>>((resolve, reject) => {
+        api
+          .put<RES, AxiosResponse<RES>, DATA>(url, data)
+          .then((data) => {
+            resolve(data);
+          })
+          .catch((err) => {
+            if (errorProcess) {
+              errorProcess(err);
+            } else {
+              // alert.openError(getErrorMessage(err));
+            }
+            reject(err);
+          });
+      });
+    },
+    /**
+     * DELETE Request
+     * @param url
+     * @param params
+     * @param errorProcess
+     * @returns
+     */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    delete: <RES = any, DATA = any>(
+      url: string,
+      params?: DATA,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      errorProcess?: (err: any) => void
+    ) => {
+      return new Promise<AxiosResponse<RES>>((resolve, reject) => {
+        api
+          .delete<RES, AxiosResponse<RES>, DATA>(url, {
+            params,
+          })
+          .then((data) => {
+            resolve(data);
+          })
+          .catch((err) => {
+            if (errorProcess) {
+              errorProcess(err);
+            } else {
+              // alert.openError(getErrorMessage(err));
+            }
+            reject(err);
+          });
+      });
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    patch: <RES = any, DATA = any>(
+      url: string,
+      data: DATA,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      errorProcess?: (err: any) => void
+    ) => {
+      return new Promise<AxiosResponse<RES>>((resolve, reject) => {
+        api
+          .patch<RES, AxiosResponse<RES>, DATA>(url, data)
+          .then((data) => {
+            resolve(data);
+          })
+          .catch((err) => {
+            if (errorProcess) {
+              errorProcess(err);
+            } else {
+              // alert.openError(getErrorMessage(err));
+            }
+            reject(err);
+          });
+      });
+    },
+  };
+};
+
+export default useHttp;
