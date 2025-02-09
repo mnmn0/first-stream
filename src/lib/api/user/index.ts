aa598eecf229ef4d841dc7d98943bde53f2aa57b
mnmn0 <minamino@mobamasu.com>
2025-02-09 23:54:07 +0900

    delete client

--- src/lib/api/user/index.ts
+++
@@ -1,9 +0,0 @@
-import { fetcher } from '@/lib/api/fetcher';
-import type { Member } from '@/types/json/user/member';
-import useSWR from 'swr';
-
-const getUsers = await fetcher<Member[]>('/api/user');
-
-const { data, error } = useSWR('/api/user', fetcher);
-
-export { getUsers };
