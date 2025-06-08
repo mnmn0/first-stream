cfd80444cd646a857204b860115ff63359369fd0
mnmn0 <minamino@mobamasu.com>
2025-06-08 20:59:04 +0900

    refactor: standardize string quotes and improve formatting in layout.tsx

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { SidebarNav } from '@/app/(admin)/_components/SidebarNav';
+import {SidebarNav} from '@/app/(admin)/_components/SidebarNav';
 
 export default function AdminLayout({
   children,
@@ -8,12 +8,12 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="space-y-6 p-6 pb-16">
-      <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-        <aside className="lg:w-1/5">
+    <div className='space-y-6 p-6 pb-16'>
+      <div className='flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0'>
+        <aside className='lg:w-1/5'>
           <SidebarNav />
         </aside>
-        <div className="flex-1">{children}</div>
+        <div className='flex-1'>{children}</div>
       </div>
     </div>
   );
